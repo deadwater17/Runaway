a8994a30eca2add37608380add92cd89d76f7b9c
deadwater <tjj.julius@gmail.com>
2025-02-24 18:49:10 +0000

    issue with movement script

---
+++ Assets/Programmers/Movement/movement.cs
@@ -0,0 +1,85 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class movement : MonoBehaviour
+{
+    //Camera 
+    public float mouseSensitivity = 2f;
+    private float verticalRotation = 0f;
+    private Transform cameraTransform;
+
+    //Player movement
+    private Rigidbody rb;
+    public float movespeed = 2f;
+    private float moveHorizontal;
+    private float moveForward;
+
+    //Player jump
+    public float jumpForce = 10f;
+    bool isGrounded = true;
+    public LayerMask groundLayer;
+    private float groundCheckTimer = 0f;
+    private float groundCheckDelay = 0.3f;
+
+    void Start()
+    {
+        rb = GetComponent<Rigidbody>();
+        rb.freezeRotation = true;
+        cameraTransform = Camera.main.transform;
+
+        Cursor.lockState = CursorLockMode.Locked;
+        Cursor.visible = false;
+    }
+    
+    void Update()
+    {
+        moveHorizontal = Input.GetAxisRaw("Horizontal");
+        moveForward = Input.GetAxisRaw("Vertical");
+
+        CameraRoatation();
+
+        if (Input.GetButtonDown("Jump") && isGrounded)
+        {
+            Jump();
+        }
+    }
+
+    void FixedUpdate()
+    {
+        Movement();
+    }
+    void Movement()
+    {
+        Vector3 movement = (transform.right * moveHorizontal + transform.forward * moveForward).normalized;
+        Vector3 targetVelocity = movement * movespeed;
+
+        Vector3 velocity = rb.velocity;
+        velocity.x = targetVelocity.x;
+        velocity.z = targetVelocity.z;
+        rb.velocity = velocity;
+
+        if (isGrounded && moveHorizontal == 0 && moveForward == 0)
+        {
+            rb.velocity = new Vector3 (0,rb.velocity.y,0);
+        }
+    }
+
+    void Jump()
+    {
+        isGrounded = false;
+        groundCheckTimer = groundCheckDelay;
+        rb.velocity = new Vector3(rb.velocity.x, jumpForce, rb.velocity.z);
+    }
+
+    void CameraRoatation()
+    {
+        float horizontalRotation = Input.GetAxis("Mouse X") * mouseSensitivity;
+        transform.Rotate (0,horizontalRotation,0);
+        
+        verticalRotation -= Input.GetAxis("Mouse Y") * mouseSensitivity;
+        verticalRotation = Mathf.Clamp(verticalRotation,-90,90);
+
+        cameraTransform.localRotation = Quaternion.Euler(verticalRotation,0,0);
+    }
+}
