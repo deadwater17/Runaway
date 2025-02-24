9035961c8d199f5e495f3fa4b4f7ab7110b9641a
YUANXIUQU <s5624234@bournemouth.ac.uk>
2025-02-24 20:17:59 +0000

    add charge up

--- Assets/Test Mechanics/Bow Mech/BowController.cs
+++ Assets/Test Mechanics/Bow Mech/BowController.cs
@@ -7,17 +7,42 @@ public class BowController : MonoBehaviour
     public GameObject Arrow_prefab;
     public Transform Arrow_spawn;
     public Transform Player;
-    void Start()
-    {
-        //arrow_prefab.transform.Rotate(90, 0, 0);
-    }
+    bool isCharging = false;
+    float velocity = 0;
+    float chargeTime = 0;
+    float minSpeed = 10;
+    float maxSpeed = 30;
+    
 
     // Update is called once per frame
     void Update()
     {
         if(Input.GetMouseButtonDown(0)) 
         {
+            isCharging = true;
+        }
+        if(isCharging) 
+        {
+            chargeTime += Time.deltaTime;
+        }
+        if(Input.GetMouseButtonUp(0)) 
+        {
+            if (chargeTime <= 1) // setting lowest velocity
+            {
+                velocity = minSpeed;
+            }
+            else if (chargeTime >= 3) // setting highest velocity
+            {
+                velocity = maxSpeed;
+            }
+            else
+            {
+                velocity = (chargeTime / 3) * maxSpeed;
+            }
             FireArrow();
+            chargeTime = 0;
+            
+            isCharging = false;
         }
     }
 
@@ -26,12 +51,9 @@ public class BowController : MonoBehaviour
         //create prefab 
         GameObject arrow = Instantiate(Arrow_prefab, Arrow_spawn.position, Quaternion.identity);
 
-        //change the orientation of original arrow
-        arrow.transform.rotation = Player.rotation; 
-        arrow.transform.Rotate(90, 0, 0);
-
         //control the velocity of arrow
+        Debug.Log(velocity);
         Rigidbody rb = arrow.GetComponent<Rigidbody>();
-        rb.velocity = Arrow_spawn.forward * 10;
+        rb.velocity = Arrow_spawn.forward * velocity;
     }
 }
