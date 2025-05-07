85798d0174bc1cf4cabf2fdadb26c277c27c15d1
YUANXIUQU <s5624234@bournemouth.ac.uk>
2025-05-07 14:55:03 +0100

    update animal navigation

---
+++ Assets/Programmers/Qu/Script/DayNightShift.cs
@@ -0,0 +1,99 @@
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEditor.Rendering.LookDev;
+using UnityEngine;
+
+public class DayNightShift : MonoBehaviour
+{
+    public Material[] skyMaterials;
+    public float fullDayDuration = 120f;
+    public float transitionDuration = 3;
+
+    private int currentIndex = 0;
+    
+    private float timer = 0f;
+
+
+    void Update()
+    {
+        timer += Time.deltaTime;
+        if(timer > fullDayDuration)
+        {
+            timer = 0f;
+        }
+        float progress = timer / fullDayDuration;
+        int newIndex = (int)(progress * skyMaterials.Length);
+        transform.rotation = Quaternion.Lerp(Quaternion.Euler(0,0,0),Quaternion.Euler(360,0,0),progress);
+        if(newIndex!=currentIndex)
+        {
+            StartCoroutine(TransitionMat(newIndex));
+            currentIndex = newIndex;
+        }
+    }
+
+    IEnumerator TransitionMat(int index)
+    {
+        Material startMat = RenderSettings.skybox;
+        Material endMat = skyMaterials[index];
+        Material temp = new Material(startMat);
+        float elapsed = 0f;
+        while(elapsed < transitionDuration)
+        {
+            elapsed += Time.deltaTime;
+            float t = elapsed / transitionDuration;
+            TransitionSkyProperties(startMat, endMat, temp, t);
+            RenderSettings.skybox = temp;
+            DynamicGI.UpdateEnvironment();
+            yield return null;
+        }
+        //RenderSettings.skybox = endMat;
+    }
+
+
+    void TransitionSkyProperties(Material start, Material end, Material target, float t)
+    {
+        string[] colorProps = new string[] {
+        "_SunDiscColor",
+        "_SunHaloColor",
+        "_HorizonLineColor",
+        "_SkyGradientTop",
+        "_SkyGradientBottom"};
+
+        foreach (string prop in colorProps)
+        {
+            if (start.HasProperty(prop) && end.HasProperty(prop))
+            {
+                target.SetColor(prop, Color.Lerp(
+                    start.GetColor(prop),
+                    end.GetColor(prop),
+                    t
+                ));
+            }
+        }
+
+
+        string[] floatProp = new string[] {
+            "_SunDiscMultiplier",
+            "_SunDiscExponent",
+            "_SunHaloExponent",
+            "_SunHaloContribution",
+            "_HorizonLineExponent",
+            "_HorizonLineContribution",
+            "_SkyGradientExponent",
+
+        };
+
+        foreach (string prop in floatProp)
+        {
+            if (start.HasProperty(prop) && end.HasProperty(prop))
+            {
+                target.SetFloat(prop, Mathf.Lerp(
+                    start.GetFloat(prop),
+                    end.GetFloat(prop),
+                    t
+                ));
+            }
+        }
+    }
+}
