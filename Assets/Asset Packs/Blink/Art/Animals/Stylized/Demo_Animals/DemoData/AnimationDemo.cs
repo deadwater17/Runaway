d00234674b0d24e780856fa847362450a7b78ac9
Finlay Rees (s5621777) <s5621777@bournemouth.ac.uk>
2025-05-19 15:18:18 +0100

    deer added

--- Assets/Asset Packs/Blink/Art/Animals/Stylized/Demo_Animals/DemoData/AnimationDemo.cs
+++ Assets/Asset Packs/Blink/Art/Animals/Stylized/Demo_Animals/DemoData/AnimationDemo.cs
@@ -1,93 +1,95 @@
-﻿using System;
-using System.Collections;
-using System.Collections.Generic;
-using TMPro;
+﻿using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.UI;
 
-public class AnimationDemo : MonoBehaviour
+namespace BLINK
 {
-
-    public enum AnimationType
+    public class AnimationDemo : MonoBehaviour
     {
-        Trigger,
-        Bool
-    }
 
-    [System.Serializable]
-    public class AnimationEntry
-    {
-        public string animationName;
-        public AnimationType type;
-    }
-
-    public List<AnimationEntry> entries = new List<AnimationEntry>();
-
-    public List<Animator> animators = new List<Animator>();
-
-    public int entryIndex;
-    public Text animationNameText;
-
-    private void Update()
-    {
-        if (Input.GetKeyDown(KeyCode.A))
+        public enum AnimationType
         {
-            NextAnimation();
+            Trigger,
+            Bool
         }
-        if (Input.GetKeyDown(KeyCode.R))
+
+        [System.Serializable]
+        public class AnimationEntry
         {
-            ReplayAnimation();
+            public string animationName;
+            public AnimationType type;
         }
-    }
 
-    public void NextAnimation()
-    {
-        entryIndex++;
-        if (entries.Count - 1 < entryIndex) entryIndex = 0;
-        animationNameText.text = entries[entryIndex].animationName;
-        PlayAnimation();
-    }
+        public List<AnimationEntry> entries = new List<AnimationEntry>();
 
-    public void PreviousAnimation()
-    {
-        entryIndex--;
-        if (entryIndex < 0) entryIndex = entries.Count-1;
-        animationNameText.text = entries[entryIndex].animationName;
-        PlayAnimation();
-    }
-    
-    public void ReplayAnimation()
-    {
-        PlayAnimation();
-    }
+        public List<Animator> animators = new List<Animator>();
 
-    private void ResetAllBool()
-    {
-        foreach (var entry in entries)
+        public int entryIndex;
+        public Text animationNameText;
+
+        private void Update()
         {
-            if (entry.type != AnimationType.Bool) continue;
-            foreach (var animator in animators)
+            if (Input.GetKeyDown(KeyCode.A))
             {
-                animator.SetBool(entry.animationName, false);
+                NextAnimation();
+            }
+
+            if (Input.GetKeyDown(KeyCode.R))
+            {
+                ReplayAnimation();
             }
         }
-    }
 
-    private void PlayAnimation()
-    {
-        ResetAllBool();
-        
-        if (entries[entryIndex].type == AnimationType.Bool)
+        public void NextAnimation()
         {
-            foreach (var animator in animators)
+            entryIndex++;
+            if (entries.Count - 1 < entryIndex) entryIndex = 0;
+            animationNameText.text = entries[entryIndex].animationName;
+            PlayAnimation();
+        }
+
+        public void PreviousAnimation()
+        {
+            entryIndex--;
+            if (entryIndex < 0) entryIndex = entries.Count - 1;
+            animationNameText.text = entries[entryIndex].animationName;
+            PlayAnimation();
+        }
+
+        public void ReplayAnimation()
+        {
+            PlayAnimation();
+        }
+
+        private void ResetAllBool()
+        {
+            foreach (var entry in entries)
             {
-                animator.SetBool(entries[entryIndex].animationName, true);
+                if (entry.type != AnimationType.Bool) continue;
+                foreach (var animator in animators)
+                {
+                    animator.SetBool(entry.animationName, false);
+                }
             }
-        } else
+        }
+
+        private void PlayAnimation()
         {
-            foreach (var animator in animators)
+            ResetAllBool();
+
+            if (entries[entryIndex].type == AnimationType.Bool)
             {
-                animator.SetTrigger(entries[entryIndex].animationName);
+                foreach (var animator in animators)
+                {
+                    animator.SetBool(entries[entryIndex].animationName, true);
+                }
+            }
+            else
+            {
+                foreach (var animator in animators)
+                {
+                    animator.SetTrigger(entries[entryIndex].animationName);
+                }
             }
         }
     }
