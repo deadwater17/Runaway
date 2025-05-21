5423b75cbe812796c374cc4e922b2ec778f106b7
Matthew Khan (s5606746) <s5606746@bournemouth.ac.uk>
2025-05-21 17:09:15 +0100

    NotebookWorkingOn

---
+++ Assets/Scripts For Designer/WorldSpaceUIClick.cs
@@ -0,0 +1,11 @@
+using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.EventSystems;
+
+public class WorldSpaceUIClick : MonoBehaviour
+{
+    public void OnMyButtonClicked()
+    {
+        Debug.Log("World Space Button Clicked!");
+    }
+}
