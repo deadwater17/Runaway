2eb31aae686fd088c3efc99d109f4ca3f5a56499
Matthew Khan (s5606746) <s5606746@bournemouth.ac.uk>
2025-05-23 17:11:17 +0100

    More Audio
    Intro Almost Fixed. Dave voices added

---
+++ Assets/DaveVoices.cs
@@ -0,0 +1,44 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DaveVoices : MonoBehaviour
+{
+    public AudioSource audioSource;
+    public AudioClip[] voicelines;
+
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    private void OnTriggerEnter(Collider other)
+    {
+        AudioClip[] clips = voicelines;
+
+        if (clips.Length == 0)
+        {
+            Debug.LogWarning("No audio clips set.");
+            return;
+        }
+        else 
+        {
+            Debug.Log("Voiceline plays");
+        AudioClip selectedClip = clips[Random.Range(0, clips.Length)];
+        audioSource.clip = selectedClip;
+        audioSource.volume = 1f;
+        audioSource.PlayOneShot(audioSource.clip);
+        }
+        
+        
+    }
+
+}
--- Assets/Programmers/Julius/Scripts/Shop/GunShop.cs
+++ Assets/Programmers/Julius/Scripts/Shop/GunShop.cs
@@ -11,6 +11,13 @@ public class GunShopUI : MonoBehaviour, IInteractable
     [SerializeField] private int cost = 100;
     private int money;
 
+
+    private AudioSource AudioSource;
+    public AudioClip voiceline;
+    public AudioClip purchase;
+
+    public GameObject subtitles;
+
     void Update()
     {
         money = int.Parse(moneyCount.text);
@@ -23,12 +30,16 @@ public class GunShopUI : MonoBehaviour, IInteractable
         {
             Debug.Log("Trying to sell");
             if (money >= cost)
-            {   
+            {
+                AudioSource.PlayOneShot(purchase);
                 money = money - cost;
                 Debug.Log(money);
                 moneyCount.text = money.ToString();
                 weaponSwitch.gunUpgraded = true;        // allow usasge of upgraded gun
                 weaponSwitch.checkGunUpgraded();
+                AudioSource.PlayOneShot(voiceline);
+                subtitles.gameObject.SetActive(true);
+                Invoke("Subtitles", 4.0f);
             }
             else
             {
@@ -40,4 +51,11 @@ public class GunShopUI : MonoBehaviour, IInteractable
             Debug.Log("Weapon is already upgraded");
         }
     }
+
+    void Subtitles()
+    {
+        subtitles.gameObject.SetActive(false);
+    }
+
+
 }
