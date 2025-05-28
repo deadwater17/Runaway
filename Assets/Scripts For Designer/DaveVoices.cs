cea19c835058070511c3ecf3383692e41c9e585f
MattKhan453 <matthew.khan453@outlook.com>
2025-05-28 22:51:40 +0100

    Subs added1

--- Assets/Scripts For Designer/DaveVoices.cs
+++ Assets/Scripts For Designer/DaveVoices.cs
@@ -7,6 +7,11 @@ public class DaveVoices : MonoBehaviour
     public AudioClip firstTimeVoiceLine;      // Special voice line for the first time
     public AudioClip[] otherVoiceLines;       // Random voice lines for subsequent entries
 
+    public GameObject dave1;
+    public GameObject dave1second;
+    public GameObject dave1third;
+    public GameObject[] subtitles;
+
     private AudioSource audioSource;
     private bool hasPlayedFirstLine = false;
 
@@ -28,6 +33,8 @@ public class DaveVoices : MonoBehaviour
             {
                 audioSource.clip = firstTimeVoiceLine;
                 audioSource.Play();
+                dave1.SetActive(true);
+                Invoke("Dave1", 2.5f);
                 hasPlayedFirstLine = true;
             }
             else if (otherVoiceLines.Length > 0)
@@ -35,7 +42,107 @@ public class DaveVoices : MonoBehaviour
                 int randomIndex = Random.Range(0, otherVoiceLines.Length);
                 audioSource.clip = otherVoiceLines[randomIndex];
                 audioSource.Play();
+                if (randomIndex == 0)
+                {
+                    subtitles[0].SetActive(true);
+                    Invoke("Dave2", 3.0f);
+                }
+                else if (randomIndex == 1)
+                {
+                    subtitles[1].SetActive(true);
+                    Invoke("Dave3", 5.0f);
+                }
+                else if (randomIndex == 2)
+                {
+                    subtitles[2].SetActive(true);
+                    Invoke("Dave4", 3.0f);
+                }
+                else if (randomIndex == 3)
+                {
+                    subtitles[4].SetActive(true);
+                    Invoke("Dave5", 4.0f);
+                }
+                else if (randomIndex == 4)
+                {
+                    subtitles[5].SetActive(true);
+                    Invoke("Dave6", 3.0f);
+                }
+                else if (randomIndex == 5)
+                {
+                    subtitles[6].SetActive(true);
+                    Invoke("Dave7", 1.9f);
+                }
+                else if (randomIndex == 6)
+                {
+                    subtitles[9].SetActive(true);
+                    Invoke("Dave8", 4.2f);
+                }
             }
         }
+    }
+
+    void Dave1()
+    {
+        dave1.SetActive(false);
+        dave1second.SetActive(true);
+        Invoke("Dave1second", 3.5f);
+    }
+    void Dave1second()
+    {
+        dave1second.SetActive(false);
+        dave1third.SetActive(true);
+        Invoke("Dave1third", 3.5f);
+    }
+    void Dave1third()
+    {
+        dave1third.SetActive(false);
+    }
+
+
+    void Dave2()
+    {
+        subtitles[0].SetActive(false);
+    }
+    void Dave3()
+    {
+        subtitles[1].SetActive(false);
+    }
+    void Dave4()
+    {
+        subtitles[2].SetActive(false);
+        subtitles[3].SetActive(true);
+        Invoke("Dave4second", 3.0f);
+    }
+    void Dave4second()
+    {
+        subtitles[3].SetActive(false);
+    }
+    void Dave5()
+    {
+        subtitles[4].SetActive(false);
+    }
+    void Dave6()
+    {
+        subtitles[5].SetActive(false);
+    }
+    void Dave7()
+    {
+        subtitles[6].SetActive(false);
+        subtitles[7].SetActive(true);
+        Invoke("Dave7second", 2.1f);
+    }
+    void Dave7second()
+    {
+        subtitles[7].SetActive(false);
+        subtitles[8].SetActive(true);
+        Invoke("Dave7third", 2.3f);
+    }
+    void Dave7third()
+    {
+        subtitles[8].SetActive(false);
+    }
+    void Dave8()
+    {
+        subtitles[9].SetActive(false);
     }
 }
