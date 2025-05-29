138bcc180c9c120132ac47d49dc135e16670956a
YUANXIUQU <s5624234@bournemouth.ac.uk>
2025-05-29 23:27:03 +0100

    delete some unused references

--- Assets/Programmers/Julius/Scripts/Script/Animal/AnimalMovement.cs
+++ Assets/Programmers/Julius/Scripts/Script/Animal/AnimalMovement.cs
@@ -3,7 +3,6 @@ using System.Collections.Generic;
 using Unity.VisualScripting;
 using UnityEngine;
 using UnityEngine.AI;
-using UnityEngine.SocialPlatforms.GameCenter;
 
 public class AnimalMovement : MonoBehaviour
 {
